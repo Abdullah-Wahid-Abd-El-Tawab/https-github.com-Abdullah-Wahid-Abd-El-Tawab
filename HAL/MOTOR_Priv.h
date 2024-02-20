13f349e901c747f01991755c45a3caee00258eac
Abdullah Wahid <160411591+Abdullah-Wahid-Abd-El-Tawab@users.noreply.github.com>
2024-02-20 09:46:51 +0200

    Add files via upload

---
+++ HAL/MOTOR_Priv.h
@@ -0,0 +1,15 @@
+#ifndef MOTOR_PRIV_H_
+#define MOTOR_PRIV_H_
+
+
+
+
+#define MOTOR_OF_PIN     2
+
+#define IN1 0
+#define IN2 1
+
+extern const DIO_Pin_t  MotorArray[NUMBER_OF_MOTORS][MOTOR_OF_PIN];
+
+
+#endif /* MOTOR_PRIV_H_ */
